23bcb57a6a74668eddca7aa737edff9e93f210d8
dietrich ayala <autonome@gmail.com>
2018-12-05 09:49:04 -0500

    add sync-to-bookmarks experiment

--- background.js
+++ background.js
@@ -1,20 +1,91 @@
+const POCKET_API_INTERVAL = 1000;
+const POCKET_BATCH_FETCH = 100;
+const POCKET_FOLDER_TITLE = 'Pocket Bookmarks';
+
 let state = {
   consumerKey: '76771-4e0cd38c1450c66a5ef1ca02',
 	accessToken: null,
-	username: ''
+	username: '',
+  pocketFolderId: null
 };
 
-(async function() {
+(async function init() {
   let storedData = await browser.storage.local.get('state');
   if (storedData && storedData.state) {
     state = storedData.state;
   }
 
   // Authenticate if necessary
+  let authenticated = false;
   if (!state.accessToken) {
-    await authenticate();
+    authenticated = await authenticate();
+  }
+  else {
+    authenticated = true;
+  }
+
+  if (!state.pocketFolderId) {
+    state.pocketFolderId = await initBookmarks();
+  }
+
+  if (authenticated) {
+    syncAll();
   }
 })();
+
+async function initBookmarks() {
+  let subtree = (await browser.bookmarks.getSubTree("unfiled_____"))[0];
+  let node = subtree.children.reduce((pf, next) => next.title == POCKET_FOLDER_TITLE, false);
+
+  if (!node) {
+    node = await browser.bookmarks.create({
+      title: POCKET_FOLDER_TITLE
+    });
+  }
+
+  return node.id;
+}
+
+async function syncAll() {
+  let count = POCKET_BATCH_FETCH;
+  let offset = 0;
+  (async function nextBatch() {
+    let results = await pocketGet({
+      state: "all",
+      detailType: "simple",
+      count: count,
+      offset: offset
+    });
+
+    let keys = Object.keys(results.list);
+    let length = keys.length;
+
+    keys.forEach(k => {
+      bookmarkIfNeeded(results.list[k]);
+    });
+
+    if (length > 0) {
+      offset += length;
+      setTimeout(() => {
+        nextBatch();
+      }, POCKET_API_INTERVAL);
+    }
+    else {
+      console.log('Sync complete!', offset)
+    }
+  })();
+}
+
+async function bookmarkIfNeeded(result) {
+  let bookmarks = await browser.bookmarks.search(result.given_url);
+  if (bookmarks.length == 0) {
+    let node = await browser.bookmarks.create({
+      title: result.given_title,
+      url: result.given_url,
+      parentId: state.pocketFolderId
+    });
+  }
+}
 
 async function pocketGet(params) {
   params.consumer_key = state.consumerKey;
@@ -39,48 +110,54 @@ async function pocketSearch(terms) {
 }
 
 async function authenticate() {
-	let reqAPIURL = 'https://getpocket.com/v3/oauth/request';
-  let redirectURL = browser.identity.getRedirectURL();
+  try {
+    let reqAPIURL = 'https://getpocket.com/v3/oauth/request';
+    let redirectURL = browser.identity.getRedirectURL();
 
-  let tokenResponse = await fetch(reqAPIURL, {
-		method: 'POST',
-		headers: {
-      'Content-Type': 'application/json; charset=UTF8',
-      'X-Accept': 'application/json'
-		},
-		body: JSON.stringify({
-			consumer_key: state.consumerKey,
-			redirect_uri: redirectURL
-		})
-	});
-  let token = await tokenResponse.json();
+    let tokenResponse = await fetch(reqAPIURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF8',
+        'X-Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        consumer_key: state.consumerKey,
+        redirect_uri: redirectURL
+      })
+    });
+    let token = await tokenResponse.json();
 
-  let authURL = 'https://getpocket.com/auth/authorize'
-    + '?request_token=' + token.code
-    + '&redirect_uri=' + encodeURIComponent(redirectURL);
+    let authURL = 'https://getpocket.com/auth/authorize'
+      + '?request_token=' + token.code
+      + '&redirect_uri=' + encodeURIComponent(redirectURL);
 
-  let result = await browser.identity.launchWebAuthFlow({
-    interactive: true,
-    url: authURL
-  });
+    let result = await browser.identity.launchWebAuthFlow({
+      interactive: true,
+      url: authURL
+    });
 
-  let authAPIURL = 'https://getpocket.com/v3/oauth/authorize';
-  let accessResponse = await fetch(authAPIURL, {
-		method: 'POST',
-		headers: {
-      'Content-Type': 'application/json; charset=UTF8',
-      'X-Accept': 'application/json'
-		},
-		body: JSON.stringify({
-			consumer_key: state.consumerKey,
-      code: token.code
-		})
-	});
-  let access = await accessResponse.json();
+    let authAPIURL = 'https://getpocket.com/v3/oauth/authorize';
+    let accessResponse = await fetch(authAPIURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF8',
+        'X-Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        consumer_key: state.consumerKey,
+        code: token.code
+      })
+    });
+    let access = await accessResponse.json();
 
-  state.accessToken = access.access_token;
-  state.username = access.username;
-  saveState();
+    state.accessToken = access.access_token;
+    state.username = access.username;
+    saveState();
+  }
+  catch (ex) {
+    return false;
+  }
+  return true;
 }
 
 async function saveState() {
